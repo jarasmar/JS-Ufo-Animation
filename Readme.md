fba2b145cbc2f01e9dd824d934927e53ed6aa85e
Jara <jarasmar@gmail.com>
2021-03-20 17:27:22 +0000

    Add basic background style

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
 # Create a UFO animation in JS Web App
+
+### Color Palette
+- Dark Purple (Sky): rgba(71, 0, 68, 1) - #470044
+- Medium Purple (Sky): rgba(100, 1, 73, 1) - #640149
+- Plum (Planet): rgba(134, 27, 74, 1) - #861B4A
+- Dark Orange (Planet): rgba(187, 71, 76, 1) - #BB474C
+- Medium Orange (Planet): rgba(226, 125, 74, 1) - #E27D4A
+- Light Orange (Planet): rgba(247, 185, 80, 1) - #F7B950
+- Yellow (Lights): rgba(249, 248, 113, 1) - #F9F871
--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const ctx = canvas.getContext("2d");
 const xBase = 400;
 const yBase = 100;
 
+// Functions for Building Planet
+
+// Planet Base
+ctx.beginPath();
+ctx.ellipse(400, 450, 430, 100, 0, Math.PI, 0);
+var grd = ctx.createLinearGradient(0, 300, 0, 400);
+grd.addColorStop(0, "rgba(247, 185, 80, 1)");
+grd.addColorStop(1, "rgba(134, 27, 74, 1)");
+ctx.fillStyle = grd;
+ctx.fill();
+
+
+
+// Functions for Building UFO
 function buildUfoBody(){
   // UFO Body top half
   ctx.beginPath();
@@ -33,47 +47,47 @@ function buildUfoBody(){
 
 function addUfoLight() {
   // Outer Shade
-  buildLightShade(120, 0.05)
+  buildLightShade(180, 0.05)
   // Middle Shade
-  buildLightShade(100, 0.1)
+  buildLightShade(160, 0.1)
   // Inner Shade
-  buildLightShade(80, 0.15)
+  buildLightShade(140, 0.15)
 }
 
 function buildLightShade(xChange, opacity) {
   ctx.beginPath();
   ctx.moveTo(xBase-20, yBase+16);
-  ctx.lineTo(xBase-xChange, yBase+200)
-  ctx.lineTo(xBase+xChange, yBase+200)
+  ctx.lineTo(xBase-xChange, yBase+500)
+  ctx.lineTo(xBase+xChange, yBase+500)
   ctx.lineTo(xBase+20, yBase+16)
-  ctx.fillStyle = `rgba(250, 250, 0, ${opacity})`
+  ctx.fillStyle = `rgba(249, 248, 113, ${opacity})`
   ctx.fill();
 }
 
 function addUfoWindows() {
   ctx.beginPath()
   ctx.ellipse(xBase-70, yBase-12, 4, 2, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(250, 250, 0)'
+  ctx.fillStyle = 'rgb(249, 248, 113)'
   ctx.fill()
   
   ctx.beginPath()
   ctx.ellipse(xBase-40, yBase-14, 6, 3.5, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(250, 250, 0)'
+  ctx.fillStyle = 'rgb(249, 248, 113)'
   ctx.fill()
 
   ctx.beginPath()
   ctx.ellipse(xBase, yBase-15, 8, 5, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(250, 250, 0)'
+  ctx.fillStyle = 'rgb(249, 248, 113)'
   ctx.fill()
 
   ctx.beginPath()
   ctx.ellipse(xBase+40, yBase-14, 6, 3.5, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(250, 250, 0)'
+  ctx.fillStyle = 'rgb(249, 248, 113)'
   ctx.fill()
 
   ctx.beginPath()
   ctx.ellipse(xBase+70, yBase-12, 4, 2, 0, 0, 2 * Math.PI);
-  ctx.fillStyle = 'rgb(250, 250, 0)'
+  ctx.fillStyle = 'rgb(249, 248, 113)'
   ctx.fill()
 }
 
